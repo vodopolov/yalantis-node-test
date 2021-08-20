2aa8726a0e42c2bc924b2bde6282f2da2009d872
Anton V <vodopolov@devoler.com>
2021-08-20 23:24:16 +0300

    Sqlite repository init commit

---
+++ src/models/user/repository/UserProfileSqliteRepository.ts
@@ -0,0 +1,82 @@
+import sqlite3 from 'sqlite3'
+import { UserProfile } from '../UserProfile'
+import { UserSavedResponse } from '../UserSavedResponse'
+import { IUserProfileRepository } from './IUserProfileRepository'
+
+export class UserProfileSqliteRepository implements IUserProfileRepository {
+  private readonly db: sqlite3.Database
+
+  public constructor() {
+    this.db = new sqlite3.Database('storage/database.db', this.createTable.bind(this))
+  }
+
+  getOne(id: number): Promise<UserProfile> {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT * FROM users WHERE id = ${id}`
+      this.db.get(query, (err, row) => {
+        if (err) {
+          return reject(err)
+        }
+        const profile = new UserProfile(row.firstName, row.lastName, row.email, row.avatarUrl)
+        profile.setId(id)
+        return resolve(profile)
+      })
+    })
+  }
+
+  getAll(): Promise<UserProfile[]> {
+    return new Promise((resolve, reject) => {
+      const query = 'SELECT * FROM Users'
+      this.db.all(query, (err, rows) => {
+        if (err) {
+          return reject(err)
+        }
+        const profiles: UserProfile[] = []
+        rows.forEach((row) => {
+          const rowProfile = new UserProfile(row.firstName, row.lastName, row.email, row.avatarUrl)
+          rowProfile.setId(row._id)
+          profiles.push(rowProfile)
+        })
+        return resolve(profiles)
+      })
+    })
+  }
+
+  delete(id: number): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+
+  update(user: UserProfile): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+
+  save(user: UserProfile): Promise<UserSavedResponse> {
+    return new Promise((resolve, reject) => {
+      const query = 'INSERT INTO Users(firstName, lastName, email, avatarUrl) VALUES(?, ?, ?, ?)'
+      this.db.run(query, [user.firstName, user.lastName, user.email, user.avatarUrl], function (err) {
+        if (err) {
+          return reject(err)
+        }
+        return resolve(new UserSavedResponse(this.lastID))
+      })
+    })
+  }
+
+  private createTable(err: Error | null) {
+    if (err) {
+      return console.error(err.message)
+    }
+    this.db.run(`CREATE TABLE IF NOT EXISTS Users(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      firstName TEXT,
+      lastName TEXT,
+      email TEXT,
+      avatarUrl TEXT)`, (err) => {
+      if (err) {
+        throw new Error(err.message)
+      } else {
+        console.log('Table was successfully created')
+      }
+    })
+  }
+}
