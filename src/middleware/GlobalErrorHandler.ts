8f9d2bf540d52f2925440ac68d90bc32445ab943
Anton V <vodopolov@devoler.com>
2021-08-20 23:21:27 +0300

    Error handling fix

--- src/middleware/GlobalErrorHandler.ts
+++ src/middleware/GlobalErrorHandler.ts
@@ -3,7 +3,19 @@ import { ExpressErrorMiddlewareInterface, Middleware } from 'routing-controllers
 @Middleware({ type: 'after' })
 export class GlobalErrorHandler implements ExpressErrorMiddlewareInterface {
   error(error: any, request: any, response: any, next: () => any) {
-    response.send({ ERROR: error })
+    response.send(
+      {
+        ERROR:
+          this.checkIfSimpleError(error) ? this.simpleErrorFormatter(error) : error
+      })
     next()
+  }
+
+  private simpleErrorFormatter(error: any): string {
+    return `${error.message} \n ${error.stack}`
+  }
+
+  private checkIfSimpleError(e: any): boolean {
+    return !!e && (e.message && !e.errors && !e.httpCode)
   }
 }
